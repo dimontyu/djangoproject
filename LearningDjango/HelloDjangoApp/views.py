f58fe4a0edb3fe6bf25b8ddbf9fd6d6a9ca14750
dima.chernyshev.1976 <dima.chernyshev.1976@mail.ru>
2019-05-07 22:30:57 +0300

    добавил index.html

--- LearningDjango/HelloDjangoApp/views.py
+++ LearningDjango/HelloDjangoApp/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
-
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
 
-    return HttpResponse(html_content)
 
