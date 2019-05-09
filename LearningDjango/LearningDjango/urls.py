0465dfa704b65b03c6ab06f3512622984834e16c
dima.chernyshev.1976 <dima.chernyshev.1976@mail.ru>
2019-05-09 21:42:34 +0300

    lj,fdbk

--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
 url(r'^about$', HelloDjangoApp.views.about, name='about'),
+
 ]
